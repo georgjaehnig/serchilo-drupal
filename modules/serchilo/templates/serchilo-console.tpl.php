bf37fa90141a34f3dbabc1859a4e8676c4031028
Georg Jaehnig <georg@jaehnig.org>
2014-06-26 00:30:38 +0200

    cleanup of unused elements, full width

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -1,26 +1,19 @@
-<div id="commandHeader">
-  <!--<a id="title" href="#" target="_new"></a>-->
-  <h1 class="title" id="page-title"><a id="title" href="#">&nbsp;</a></h1>
-  <a href="#" target="_new" id="namespace" class="namespace"></a><span id="tags">  </span>
-
-  <!--<a id="closeButton" href="#">close</a>-->
-</div>
-
-<div id="search">
-  <form action="?" method="get" id="serchilo-command-query-form" accept-charset="UTF-8" role="form" class="form-inline">
-  <div class="form-group">
-    <input type="hidden" class="form-control" id="searchInputBackground" name="query" value="<?php echo $query ?>" autocomplete="off" >
-    <input type="search" class="form-control" id="searchInput" name="query" value="<?php echo $query ?>" autocomplete="off" >
+<div class="container">
+  <div id="search">
+    <form action="?" method="get" id="serchilo-command-query-form" accept-charset="UTF-8" class="form" role="form">
+      <div class="form-group">
+        <div class="col-md-10 col-xs-10 col-sm-10 col-lg-10 ">
+          <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off">
+        </div>
+      <button id="searchSubmit" type="submit" class="btn btn-default col-xs-2 col-sm-2 col-md-2 col-lg-2 ">Go</button>
+      </div>
+    </form>
+    &nbsp;
   </div>
-  <input type="hidden" name="default_keyword" value="<?php echo $default_keyword ?>" >
-  <button type="submit" class="btn btn-default" id="searchSubmit">Go</button>
-  </form>
-&nbsp;
-</div>
 
-
-<div id="help">
-Start with a <a href="<?php echo $commands_of_current_namespaces_url ?>">keyword</a>, continue with your search.
-Or watch a <a href="http://www.youtube.com/watch?v=lNx4CFM-P2M" target="_blank">screencast video</a> to learn more.
+  <div id="help">
+    Start with a <a href="<?php echo $commands_of_current_namespaces_url ?>">keyword</a>, continue with your search.
+    Or watch a <a href="http://www.youtube.com/watch?v=lNx4CFM-P2M" target="_blank">screencast video</a> to learn more.
+  </div>
 </div>
 
