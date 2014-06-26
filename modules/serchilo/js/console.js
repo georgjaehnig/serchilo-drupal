4de22e312e5baadbd13244d46b55f5a37d8be07e
Georg Jaehnig <georg@jaehnig.org>
2014-06-26 15:06:42 +0200

    cleanup of unused code

--- modules/serchilo/js/console.js
+++ modules/serchilo/js/console.js
@@ -1,24 +1,6 @@
-function installSearchPlugin()
-{
-	window.external.AddSearchProvider(
-		"http://" + location.host + "/opensearch" + location.pathname + location.search
-	);
-}
-
 (function ($) {
 
   var called = false;
-  var searchInput;
-  var searchInputBackground;
-  /*
-  var examples = [
-    ['g berlin', 'Google search for "berlin"'],
-    ['w berlin', 'Wikipedia article about Berlin'],
-    ['db berlin, hamburg', 'Nächste Verbindung von Berlin nach Hamburg']
-  ]; */
-  var typewriteTimeout;
-  var typewriting = false;
-
 
   // command array indexes
   var ID                = 0;
@@ -29,255 +11,6 @@ function installSearchPlugin()
   var REACHABLE         = 5;
   var URL               = 6;
 
-  var commandSelectedIndex = -1;
-
-  function startTypewrite() {
-
-    //var examples = Drupal.settings.serchilo.typewrite_examples;
-    function typewrite() {
-
-      if (examplesIndex == examples.length) {
-        examplesIndex = 0;
-      }
-      query = examples[examplesIndex][0];
-      description = examples[examplesIndex][1];
-      // type query
-      if (queryPosition < query.length) {
-        searchInput.val(query.substring(0, queryPosition + 1));
-        queryPosition = queryPosition + 1;
-        typewriteTimeout = setTimeout( function() { typewrite(); }, 100 );
-        return;
-      }
-      // show description
-      if (queryPosition == query.length) {
-        searchInputBackground.val(description);
-        queryPosition = queryPosition + 1;
-        typewriteTimeout = setTimeout( function() { typewrite(); }, 3000 );
-        return;
-      }
-      // clear all
-      if (queryPosition > query.length) {
-        searchInput.val('');
-        searchInputBackground.val('');
-        queryPosition = 0;
-        examplesIndex = examplesIndex + 1;
-        typewriteTimeout = setTimeout( function() { typewrite(); }, 100 );
-        return;
-      }
-    }
-
-    examplesIndex = 0;
-    queryPosition = 0; 
-    typewrite();
-    typewriting = true;
-  }
-
-  function stopTypewrite() {
-    if (typewriting) {
-      clearTimeout(typewriteTimeout);
-      searchInput.val('');
-      searchInputBackground.val('');
-      searchInputBackground.css('padding-right', 0);
-      searchInputBackground.css('text-align', 'left');
-      typewriting = false;
-    }
-  }
-
-  function parseQuery(queryString)
-  {
-    //			bg				qS
-    // ''						-2
-    // 'f'		'f'		' x,y'	-1
-    // 'f '		'f '	'x,y'	0
-    // 'f a'	'f a'	',y'	1
-    // 'f a,'	'f a,'	'y'		2
-    // 'f a,b'	'f a,b'	''		2
-
-    var state = -2;
-    var keyword = '';
-    var args = [];
-    var namespace = '';
-		// syntax: keyword, args
-		// or: namespace.keyword args
-    matches = queryString.match( /^\s*((\S*)\.)?(\S*)( *)(.*)$/ )
-
-    keyword = matches[3];
-    namespace = matches[2];
-    if ( matches[5] != '' ) {
-      args = matches[5].split(',');
-    }
-    if ( keyword != '' ) {
-      state = -1;	
-    } 
-    if ( matches[4] != '' ) {
-      state = 0;
-    } 
-    if ( args.length > 0 ) {
-      state = args.length;	
-    }
-    query = {
-      'state': state, 
-      'keyword': keyword,
-      'args': args,
-      'namespace': namespace,
-    };
-    return query;
-  }
-
-  function updateInputBackground()
-  {
-    command = Drupal.settings.command;
-    if (command == null) {
-      // make sure background is empty
-      unselectCommand();
-      return;
-    }
-    query = parseQuery( searchInput.val());
-    if (query.state < 0) {
-      unselectCommand();
-      return;
-    }
-    queryString = command.keyword + ' ' + query.args.join(',');
-    if (command.reachable == false) {
-      queryString = command.namespace + '.' + queryString;
-    }
-    searchInput.val( queryString );
-
-    var bgVal = queryString;
-    if ( query.state == -1 ) {
-      bgVal += ' ';	
-    }
-    if ( 
-      ( query.state > 0 ) && 
-      ( command.argument_names.split(',').length > query.args.length ) && 
-      ( query.args[query.args.length-1] != '' )
-       ) {
-      bgVal += ',';	
-    }
-    if (
-      ( query.args.length > 1 ) &&
-      ( query.args[query.args.length-1] == '' )
-       ) {
-      query.state = query.state-1;
-    }
-    bgVal += command.argument_names.split(',').slice(Math.max(0,query.state)).join(',');
-    searchInputBackground.val( bgVal );
-  }
-
-  function selectCommand( id, reachable ) {
-
-    $.getJSON(Drupal.settings.basePath + 'ajax/command/' + id, function(command) {
-      if (command.id == null) {
-        console.log('No command with ID ' + id + ' found.');
-        return; 
-      }
-
-      command.reachable = reachable;
-
-      $( '#title' ).attr('href', encodeURI( Drupal.settings.basePath + 'command/' + command.id  ));
-
-      $( '#title' ).text( command.title);
-
-      $( '#namespace' ).text(command.namespace);
-      $( '#namespace' ).attr('href', encodeURI( Drupal.settings.basePath + 'commands?namespace=' + command.namespace ));
-
-      $('#tags').empty();
-      $.each(
-        command.tags, 
-        function(i,tag) {
-          $('#tags').append(
-            $(
-              '<a>', 
-              {
-                'text': tag,
-                'class': 'tag',
-                'href': encodeURI( Drupal.settings.basePath + 'commands?tags=' + tag),
-                'target': '_new'
-              }
-            )
-          );
-        }
-      );
-
-      $('#commandHeader').css('visibility', 'visible');
-
-      jQuery.each(command.examples, function(key, value) {
-        arguments = value[0];
-        description = value[1];
-        queryA = $('<a>');
-        queryA.text(command.keyword + ' ' + arguments);
-        queryA.attr('href', encodeURI( 
-            '?query=' + command.namespace + '.' + command.keyword + ' ' + arguments
-
-          )
-        );
-        queryDT = $('<dt>');
-        queryDT.append(queryA);
-        $('#examples dl').append(queryDT);
-
-        descriptionDD = $('<dd>');
-        descriptionDD.text(description);
-        $('#examples dl').append(descriptionDD);
-
-      } );
-      $('#examples').show();
-
-      query = parseQuery( searchInput.val());
-      queryString = command.keyword + ' ' + query.args.join(',');
-      if (command.reachable == false) {
-        queryString = command.namespace + '.' + queryString;
-      }
-      searchInput.val( queryString );
-
-      Drupal.settings.command = command;
-
-      updateInputBackground();
-
-      searchInput.autocomplete('option', 'disabled', true );
-      return;
-    });
-
-  }
-  function hideOnSearch() {
-    $('.hide-on-search').hide();
-  }
-
-  function hideCommandElements() {
-    $('#commandHeader').css('visibility', 'hidden');
-    $('#examples').hide();
-    $('#examples dl').empty();
-
-  }
-
-  function unselectCommand() {
-  
-    hideCommandElements();
-
-    Drupal.settings.command = null;
-    searchInputBackground.val('');
-
-    searchInput.autocomplete('option', 'disabled', false);
-    searchInput.autocomplete('search');
-  }
-
-  function emptyInput() {
-    searchInput.val('');
-    searchInputBackground.val('');
-  }
-
-  function command_id_to_index(id) {
-    for (var i=0; i<commands.length; i++) {
-      if ( commands[i][ID] == id ) {
-          return commands[i][INDEX]; 
-      }
-    }
-    return null;
-  }
-
-  function isCommandSelected() {
-    return commandSelectedIndex > -1; 
-  }
-
   Drupal.behaviors.serchilo = {
     attach: function (context, settings) {
 
@@ -285,7 +18,7 @@ function installSearchPlugin()
         return; 
       }
 
-      searchInput = $('#searchInput');
+      var searchInput = $('#searchInput');
       searchInputBackground = $('#searchInputBackground');
 
       autocomplete = searchInput.autocomplete({
@@ -314,11 +47,7 @@ function installSearchPlugin()
           // prevents input being emptied when scrolling over commands in autocomplete
           return false;
         },
-
-        search: function(event, ui) {
-          hideOnSearch();
-        }
-
+      
       })
       .data('uiAutocomplete')._renderItem = function( ul, item ) {
 
@@ -368,50 +97,9 @@ function installSearchPlugin()
           .appendTo( ul );
       };
 
-  /*
-      // hide all unecessary elements
-      hideCommandElements();
-
-      // don't start typewrite when query is already filled
-      // (for instance from a failed query before)
-      if (searchInput.val() == '') {
-        //startTypewrite(examples);
-      } 
-
-      // no round corners
       searchInput.autocomplete('widget').removeClass('ui-corner-all');
-
-      // set focus on input
       searchInput.focus();
 
-      // only for html5 browsers
-      searchInput.bind('input', function() { 
-        updateInputBackground();
-      });
-
-      searchInput.click(function(){
-        //stopTypewrite();
-      });
-
-      searchInput.keydown(function(event) {
-        //stopTypewrite();
-      });
-
-      // fallback for non-html5 browsers
-      searchInput.keyup(function(event) {
-        // if there's an input event we can get out of here
-        if (typeof(searchInput.data('events').input) !== 'undefined') {
-          return;
-        }
-        updateInputBackground();
-        if (27 == event.keyCode) {
-          emptyInput();
-          unselectCommand();
-          //searchInput.autocomplete('search');
-        }
-      }); 
-
-    */
       called = true;
     }
   }
