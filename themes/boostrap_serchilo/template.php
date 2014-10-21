4f53ba7d18ae408a975552ca229dab87200fd1cf
Georg Jaehnig <georg@jaehnig.org>
2014-10-21 19:15:14 +0200

    Wrap namespaces block into a Well.

--- themes/boostrap_serchilo/template.php
+++ themes/boostrap_serchilo/template.php
@@ -4,3 +4,16 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function bootstrap_serchilo_preprocess_block(&$vars) {
+  
+  switch ($vars['block_html_id']) {
+  case 'block-serchilo-namespaces':
+    $vars['classes_array'][] = 'well';
+    $vars['classes_array'][] = 'well-sm';
+    break;
+  }
+}
