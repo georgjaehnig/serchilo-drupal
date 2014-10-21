650e4ff0f0a50f35aaf15dba18d73a11f4ddeffa
Georg Jaehnig <georg@jaehnig.org>
2014-10-21 23:42:01 +0200

    Render examples as <ol>.

---
+++ themes/boostrap_serchilo/templates/field--field-example.tpl.php
@@ -0,0 +1,25 @@
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if (!$label_hidden): ?>
+    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+  <?php endif; ?>
+  <div class="field-items"<?php print $content_attributes; ?>>
+
+    <dl class="dl-horizontal">
+
+    <?php foreach ($items as $delta => $item): ?>
+    <!-- TODO: make this a proper link -->
+      <dt><a href="#" class="btn btn-default">
+      <?php
+        print current($item['entity']['field_collection_item'])['field_example_arguments'][0]['#markup'];
+      ?></a></dt>
+      <dd>
+      <?php
+        print current($item['entity']['field_collection_item'])['field_example_description'][0]['#markup'];
+      ?>
+      </dd>
+    <?php endforeach ?>
+
+    </dl>
+
+  </div>
+</div>
