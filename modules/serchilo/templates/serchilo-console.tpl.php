efa23aadb80353aef1ed158d25b5dde9c2dfdb58
Georg Jaehnig <georg@jaehnig.org>
2014-10-24 00:43:54 +0200

    Console 'Go' button as btn-primary.

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -5,7 +5,7 @@
         <div class="col-md-10 col-xs-10 col-sm-10 col-lg-10 ">
           <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off">
         </div>
-      <button id="searchSubmit" type="submit" class="btn btn-default col-xs-2 col-sm-2 col-md-2 col-lg-2 ">Go</button>
+      <button id="searchSubmit" type="submit" class="btn btn-primary col-xs-2 col-sm-2 col-md-2 col-lg-2 ">Go</button>
       </div>
     </form>
     &nbsp;
