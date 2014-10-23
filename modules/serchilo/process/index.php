a2a642e5de7a5842ff4fda53079ac33da8dc15af
Georg Jaehnig <georg@jaehnig.org>
2014-10-23 15:07:40 +0200

    Highest argument_count first.

--- modules/serchilo/process/index.php
+++ modules/serchilo/process/index.php
@@ -415,6 +415,8 @@ function serchilo_find_shortcut($keyword, $argument_count, $namespace_ids) {
     AND
     namespace_id IN (" . join(',', $namespace_ids) .  ")
     ORDER BY
+    # Highest argument_count first
+    argument_count DESC,
     " . 
     join(',', array_map(function($v) { return "namespace_id = " . (int) $v . " DESC";  }, array_reverse($namespace_ids))) 
     . "
