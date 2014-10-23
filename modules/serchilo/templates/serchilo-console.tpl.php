37fc76134ea58ba16bf4d590a26958ef466f290d
Georg Jaehnig <georg@jaehnig.org>
2014-10-24 01:12:51 +0200

    Console: Use input group.

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -1,14 +1,14 @@
 <div class="container">
-  <div id="search">
-    <form action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form" role="form">
-      <div class="form-group">
-        <div class="col-md-10 col-xs-10 col-sm-10 col-lg-10 ">
-          <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off">
-        </div>
-      <button id="searchSubmit" type="submit" class="btn btn-primary col-xs-2 col-sm-2 col-md-2 col-lg-2 ">Go</button>
-      </div>
-    </form>
-    &nbsp;
-  </div>
+  <form role="form" action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form">  
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="input-group input-group-lg">
+              <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
+            <div class="input-group-btn">
+              <button type="submit" class="btn btn-primary">Go</button>
+            </div><!-- /btn-group -->
+          </div><!-- /input-group -->
+        </div><!-- /.col-xs-12 -->
+      </div><!-- /.row -->
+  </form>
 </div>
-
