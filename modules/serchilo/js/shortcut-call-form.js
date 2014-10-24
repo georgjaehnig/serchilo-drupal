2c3b5075ca96de7db211ece954a9031efc07f1ce
Georg Jaehnig <georg@jaehnig.org>
2014-10-24 02:41:35 +0200

    Set argument inputs to correct width via JS.

---
+++ modules/serchilo/js/shortcut-call-form.js
@@ -0,0 +1,21 @@
+
+jQuery(document).ready(function($) {
+
+  // Set widths of text inputs.
+
+  // Get current widths.
+  var form_width    = $('#serchilo-shortcut-call-form').outerWidth();
+  var keyword_width = $('#serchilo-shortcut-call-form div.keyword').outerWidth();
+  var submit_width  = $('#edit-submit').outerWidth();
+
+  // Calculate with for single text input.
+  var argument_width_total  = form_width - keyword_width - submit_width - 20;
+  var argument_count        = $('#serchilo-shortcut-call-form input.form-text').length;
+  var argument_width_single = Math.round(argument_width_total / argument_count) - 5;
+
+  // Set widths for all single text inputs.
+  $('#serchilo-shortcut-call-form input.form-text').each(function() {
+    $(this).outerWidth(argument_width_single);
+  });
+
+});
