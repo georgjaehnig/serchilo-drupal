ddbb01ad17c1124fedd71640b9a5f7bbefac14f0
Georg Jaehnig <georg@jaehnig.org>
2014-10-28 22:04:03 +0100

    On small screens, set input width to 100%.

--- modules/serchilo/js/shortcut-call-form.js
+++ modules/serchilo/js/shortcut-call-form.js
@@ -12,17 +12,24 @@ jQuery(document).ready(function($) {
 
 function serchilo_set_input_widths($) {
 
-  // Set widths of text inputs.
+  if ($('body').outerWidth() >= 768 ) {
 
-  // Get current widths.
-  var form_width    = $('#serchilo-shortcut-call-form').outerWidth();
-  var keyword_width = $('#serchilo-shortcut-call-form div.keyword').outerWidth();
-  var submit_width  = $('#edit-submit').outerWidth();
+    // Set widths of text inputs.
 
-  // Calculate with for single text input.
-  var argument_width_total  = form_width - keyword_width - submit_width - 20;
-  var argument_count        = $('#serchilo-shortcut-call-form input.form-text').length;
-  var argument_width_single = Math.round(argument_width_total / argument_count) - 5;
+    // Get current widths.
+    var form_width    = $('#serchilo-shortcut-call-form').outerWidth();
+    var keyword_width = $('#serchilo-shortcut-call-form div.keyword').outerWidth();
+    var submit_width  = $('#edit-submit').outerWidth();
+
+    // Calculate with for single text input.
+    var argument_width_total  = form_width - keyword_width - submit_width - 20;
+    var argument_count        = $('#serchilo-shortcut-call-form input.form-text').length;
+    var argument_width_single = Math.round(argument_width_total / argument_count) - 5;
+
+  }
+  else {
+    var argument_width_single = '100%';
+  }
 
   // Set widths for all single text inputs.
   $('#serchilo-shortcut-call-form input.form-text').each(function() {
