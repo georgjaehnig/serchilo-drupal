a67b073821b58036341800ad407589493711d24c
Georg Jaehnig <georg@jaehnig.org>
2014-11-26 15:32:22 +0100

    Get extra_namespace_name.

--- modules/serchilo/process/index.php
+++ modules/serchilo/process/index.php
@@ -112,6 +112,7 @@ function serchilo_populate_environment(&$env) {
       $env = serchilo_handle_query_from_request() + $env;
     } else {
       $env['keyword']        = trim(serchilo_array_value($_GET, 'keyword'));
+      $env = serchilo_get_extra_namespace_from_keyword($env['keyword']) + $env;
       $env['argument_count'] = serchilo_array_value($_GET, 'argument_count', 0);
     }
     $env['path_elements_offset'] = 1;
