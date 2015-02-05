36ab3b9155ecae167a7d4cd12e8906766f3d1868
Georg Jaehnig <georg@jaehnig.org>
2015-02-05 14:27:56 +0100

    Remove as apparently not needed. Continue work with #89.

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -1,7 +1,7 @@
 <div class="container">
   <form role="form" action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form">  
       <div class="row">
-        <div class="col-xs-12">
+        <div class=""> <!-- Removed: col-xs-12 -->
           <div class="input-group input-group-lg">
               <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
             <div class="input-group-btn">
