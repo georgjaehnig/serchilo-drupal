dd030ddfa79084858b0f3aa81dab6d60c865ca6b
Georg Jaehnig <georg@jaehnig.org>
2015-04-26 15:36:10 +0200

    Display namespaces as 1 link, and smaller.

--- themes/boostrap_serchilo/template.php
+++ themes/boostrap_serchilo/template.php
@@ -8,23 +8,6 @@
 /**
  * Implements hook_preprocess_HOOK().
  */
-function bootstrap_serchilo_preprocess_block(&$vars) {
-  
-  switch ($vars['block_html_id']) {
-
-  case 'block-serchilo-namespaces':
-
-    // Put a well around namespaces block.
-    $vars['classes_array'][] = 'well';
-    $vars['classes_array'][] = 'well-sm';
-
-    break;
-  }
-}
-
-/**
- * Implements hook_preprocess_HOOK().
- */
 function bootstrap_serchilo_preprocess_button(&$vars) {
   
   switch ($vars['element']['#button_type']) {
