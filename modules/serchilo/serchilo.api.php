e931df4759f18323babfaa1e90f1b8c6cfc9524d
Georg Jaehnig <georg@jaehnig.org>
2015-07-15 21:35:52 +0200

    Doc for hook_serchilo_console_examples(), close #99.

---
+++ modules/serchilo/serchilo.api.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the diff module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Allow modules to alter the console examples.
+ *
+ * @param array $shortcut_ids
+ *   The current draft of shortcut ids to show as examples.
+ * @param array $context
+ *   An array with context information, currently containing:
+ *   - namespace_ids: Array with the IDs of the current namespaces.
+ */
+function hook_serchilo_console_examples_alter(&$shortcut_ids, $context) {
+
+  // Completely overwrite the current draft
+  // and set shortcuts with the IDs 1 and 2 as examples.
+  $shortcut_ids = array(1,2);
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
