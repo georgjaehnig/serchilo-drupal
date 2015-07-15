65bdb5b28f75fcae6db5b48bbe621b11aa12216d
Georg Jaehnig <georg@jaehnig.org>
2015-07-15 21:23:33 +0200

    Whitespace.

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -1,13 +1,13 @@
-  <form role="form" action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form">  
-        <div class=""> <!-- Removed: col-xs-12 -->
-          <div class="input-group input-group-lg">
-              <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
-            <div class="input-group-btn">
-              <button type="submit" class="btn btn-primary">Go</button>
-            </div><!-- /btn-group -->
-            <?php foreach ($get_parameters as $name=>$value): ?>
-              <input type="hidden" name="<?php echo $name ?>" value="<?php echo $value ?>"/>
-            <?php endforeach; ?>
-          </div><!-- /input-group -->
-        </div><!-- /.col-xs-12 -->
-  </form>
+<form role="form" action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form">  
+  <div class=""> <!-- Removed: col-xs-12 -->
+    <div class="input-group input-group-lg">
+        <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
+      <div class="input-group-btn">
+        <button type="submit" class="btn btn-primary">Go</button>
+      </div><!-- /btn-group -->
+      <?php foreach ($get_parameters as $name=>$value): ?>
+        <input type="hidden" name="<?php echo $name ?>" value="<?php echo $value ?>"/>
+      <?php endforeach; ?>
+    </div><!-- /input-group -->
+  </div><!-- /.col-xs-12 -->
+</form>
