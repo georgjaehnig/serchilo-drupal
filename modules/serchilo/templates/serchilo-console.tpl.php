a2beadde23c012ded27309cd78129af43ac18741
Georg Jaehnig <georg@jaehnig.org>
2015-07-15 21:24:41 +0200

    Whitespace.

--- modules/serchilo/templates/serchilo-console.tpl.php
+++ modules/serchilo/templates/serchilo-console.tpl.php
@@ -1,11 +1,11 @@
 <form role="form" action="?" method="get" id="serchilo-shortcut-query-form" accept-charset="UTF-8" class="form">  
-    <div class="input-group input-group-lg">
-        <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
-      <div class="input-group-btn">
-        <button type="submit" class="btn btn-primary">Go</button>
-      </div><!-- /btn-group -->
-      <?php foreach ($get_parameters as $name=>$value): ?>
-        <input type="hidden" name="<?php echo $name ?>" value="<?php echo $value ?>"/>
-      <?php endforeach; ?>
-    </div><!-- /input-group -->
+  <div class="input-group input-group-lg">
+      <input id="searchInput" type="text" class="form-control" name="query" value="<?php echo $query ?>" autocomplete="off" />
+    <div class="input-group-btn">
+      <button type="submit" class="btn btn-primary">Go</button>
+    </div><!-- /btn-group -->
+    <?php foreach ($get_parameters as $name=>$value): ?>
+      <input type="hidden" name="<?php echo $name ?>" value="<?php echo $value ?>"/>
+    <?php endforeach; ?>
+  </div><!-- /input-group -->
 </form>
