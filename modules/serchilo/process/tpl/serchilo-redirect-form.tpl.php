4cad7d2514294536a804aca86258523a61372454
Georg Jaehnig <georg@jaehnig.org>
2015-07-23 10:44:15 +0200

    POST param charsets=UTF-8.

--- modules/serchilo/process/tpl/serchilo-redirect-form.tpl.php
+++ modules/serchilo/process/tpl/serchilo-redirect-form.tpl.php
@@ -2,7 +2,7 @@
        "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <body onload="document.getElementsByTagName('form').item(0).submit();">
-<form action="<?php echo $url ?>" method="post">
+<form action="<?php echo $url ?>" method="post" accept-charset="utf-8">
 <?php foreach($post_parameters as $post_key=>$post_value): ?>
   <input type="hidden" name="<?php echo $post_key; ?>" value="<?php echo $post_value; ?>" >
 <?php endforeach; ?>
