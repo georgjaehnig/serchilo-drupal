ee0096f02baac505a59015fc638bc568be5373a9
Georg Jaehnig <georg@jaehnig.org>
2015-07-29 17:16:36 +0200

    Separated content from code.

--- modules/serchilo/js/shortcut.form.field_namespace.js
+++ modules/serchilo/js/shortcut.form.field_namespace.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function () { 
-  var link = jQuery("<a style=\"cursor: pointer; cursor: hand;\">Use my user namespace</a>");
+  var link = jQuery("<a style=\"cursor: pointer; cursor: hand;\">" + Drupal.settings.serchilo.link_text + "</a>");
   link.click(function() {
-    jQuery("#edit-field-namespace-und").val(Drupal.settings.serchilo.user_name);
+    jQuery("#edit-field-namespace-und").val(Drupal.settings.serchilo.namespace_name);
   });
   jQuery(".form-item-field-namespace-und").append(link);
 });
