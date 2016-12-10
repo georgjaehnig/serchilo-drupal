5f6e1487f47eb4bd178e53f6b89ea1cb24a9178c
Georg Jaehnig <georg@jaehnig.org>
2016-12-10 23:44:00 +0100

    Indent.

--- modules/serchilo/process/telegram/Commands/GenericmessageCommand.php
+++ modules/serchilo/process/telegram/Commands/GenericmessageCommand.php
@@ -52,48 +52,35 @@ class GenericmessageCommand extends SystemCommand
 
         $env['query'] = $message->getText();
 
-    $env = serchilo_parse_query($env['query']) + $env;
+        $env = serchilo_parse_query($env['query']) + $env;
 
-    // TODO: Copied this from 
-    // serchilo_populate_environment(&$env)
-    // case SERCHILO_NAMESPACES_PATH_AFFIX:
-    // Needs to be refactored.
+        // TODO: Copied this from 
+        // serchilo_populate_environment(&$env)
+        // case SERCHILO_NAMESPACES_PATH_AFFIX:
+        // Needs to be refactored.
 
-    // Add extra_namespace to namespace_names.
-    if (!empty($env['extra_namespace_name'])) {
-      $env['namespace_names'][] = $env['extra_namespace_name'];
-    }
-    // Get namespace_ids from namespace_names.
-    $env['namespace_ids'] = array_map('serchilo_get_namespace_id', $env['namespace_names']);
+        // Add extra_namespace to namespace_names.
+        if (!empty($env['extra_namespace_name'])) {
+          $env['namespace_names'][] = $env['extra_namespace_name'];
+        }
+        // Get namespace_ids from namespace_names.
+        $env['namespace_ids'] = array_map('serchilo_get_namespace_id', $env['namespace_names']);
 
         //error_log(json_encode($env));
 
-  $output = serchilo_get_output($env);
-  //serchilo_telegram_respond($env, $telegram, $output);
+        $output = serchilo_get_output($env);
+        //serchilo_telegram_respond($env, $telegram, $output);
 
-  if ($output['status']['found']) {
-    $url = $output['url']['final'];
-    serchilo_log_shortcut_call($output['#shortcut'], $env, $output['status']['default_keyword_used']);
-  } else {
-    $text = 'Error: No shortcut found.';
-  }
+        if ($output['status']['found']) {
+          $url = $output['url']['final'];
+          serchilo_log_shortcut_call($output['#shortcut'], $env, $output['status']['default_keyword_used']);
+        } else {
+          $text = 'Error: No shortcut found.';
+        }
 
         return Request::sendMessage([
           'chat_id' => $message->getChat()->getId(),
           'text'    => (!empty($url) ? $url : $text), // json_encode($env), // $message->getText(),
         ]);
-        return;
-
-        //If a conversation is busy, execute the conversation command after handling the message
-        $conversation = new Conversation(
-            $this->getMessage()->getFrom()->getId(),
-            $this->getMessage()->getChat()->getId()
-        );
-        //Fetch conversation command if it exists and execute it
-        if ($conversation->exists() && ($command = $conversation->getCommand())) {
-            return $this->telegram->executeCommand($command, $this->update);
-        }
-
-        return Request::emptyResponse();
     }
 }
