e37e0daa37256e23dfe77c608c0f8f7788b657ec
Georg Jaehnig <georg@jaehnig.org>
2016-12-10 23:08:47 +0100

    Disable default commands.

---
+++ modules/serchilo/process/telegram/Commands/EchoCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+
+class EchoCommand extends SystemCommand
+{
+}
---
+++ modules/serchilo/process/telegram/Commands/SlapCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+
+class SlapCommand extends SystemCommand
+{
+}
