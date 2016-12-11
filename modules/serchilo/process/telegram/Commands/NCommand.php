929a3062213b6679a2e31ee9604f4370f5458ca4
Georg Jaehnig <georg@jaehnig.org>
2016-12-11 02:38:47 +0100

    Cleanup.

--- modules/serchilo/process/telegram/Commands/NCommand.php
+++ modules/serchilo/process/telegram/Commands/NCommand.php
@@ -38,46 +38,5 @@ class NCommand extends UserCommand
           'chat_id' => $message->getChat()->getId(),
           'text'    => $text
         ]);
-
-
-        $message = $this->getMessage();
-        $chat_id = $message->getChat()->getId();
-
-        $message_id = $message->getMessageId();
-        $command = trim($message->getText(true));
-
-        //Only get enabled Admin and User commands
-        $commands = array_filter($this->telegram->getCommandsList(), function ($command) {
-            return (!$command->isSystemCommand() && $command->isEnabled());
-        });
-
-        //If no command parameter is passed, show the list
-        if ($command === '') {
-            $text = $this->telegram->getBotName() . ' v. ' . $this->telegram->getVersion() . "\n\n";
-            $text .= 'Commands List:' . "\n";
-            foreach ($commands as $command) {
-                $text .= '/' . $command->getName() . ' - ' . $command->getDescription() . "\n";
-            }
-
-            $text .= "\n" . 'For exact command help type: /help <command>';
-        } else {
-            $command = str_replace('/', '', $command);
-            if (isset($commands[$command])) {
-                $command = $commands[$command];
-                $text = 'Command: ' . $command->getName() . ' v' . $command->getVersion() . "\n";
-                $text .= 'Description: ' . $command->getDescription() . "\n";
-                $text .= 'Usage: ' . $command->getUsage();
-            } else {
-                $text = 'No help available: Command /' . $command . ' not found';
-            }
-        }
-
-        $data = [
-            'chat_id'             => $chat_id,
-            'reply_to_message_id' => $message_id,
-            'text'                => $text,
-        ];
-
-        return Request::sendMessage($data);
     }
 }
